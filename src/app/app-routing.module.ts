414455819acea36c944a16d586f168a56ac5b560
Anuja Kate <kate.anuja15@gmail.com>
2021-05-09 20:16:12 +0530

    party light page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'party-light',
+    loadChildren: () => import('./party-light/party-light.module').then( m => m.PartyLightPageModule)
+  },
 ];
 
 @NgModule({
---
+++ src/app/party-light/party-light.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-party-light',
+  templateUrl: './party-light.page.html',
+  styleUrls: ['./party-light.page.scss'],
+})
+export class PartyLightPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
